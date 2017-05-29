dcdea48684b0e341d5e11e377cbb7d16028a2871
SeriousD <dennis.ernst1990@gmx.de>
2017-05-29 11:15:41 +0200

    improve plot&calc
    prettier plots, automatic CSV loading etc.

--- Projektbericht/gfx/just/plotAndCalc.py
+++ Projektbericht/gfx/just/plotAndCalc.py
@@ -9,7 +9,6 @@ Created on Tue May  9 18:17:42 2017
 import pandas as pd
 import numpy as np
 
-
 import matplotlib as mpl
 mpl.use("pgf")
 pgf_with_rc_fonts = {
@@ -18,11 +17,13 @@ pgf_with_rc_fonts = {
     "font.sans-serif": ["DejaVu Sans"], # use a specific sans-serif font
 }
 mpl.rcParams.update(pgf_with_rc_fonts)
-#pd.set_option('display.mpl_style', 'default') # Make the graphs a bit prettier
-#pd.figsize(15, 5)
+
+
+
 import matplotlib.pyplot as plt
 
 
+plt.figure(figsize=(4.5,2.5))
 name = "30kV"
 
 
@@ -35,11 +36,13 @@ A = np.vstack([x, np.ones(len(x))]).T
 w1 = np.linalg.lstsq(A,y)[0] # obtaining the parameters
 print(w1[0],w1[1])
 reg1 = w1[0]*x + w1[1]
-fig.suptitle('Messung 30kV')
-plt.xlabel('U1/V')
-plt.ylabel('U2/V')
+fig.suptitle(r'Messung 9,89 k\Omega')
+plt.xlabel(r'U_1/V')
+plt.ylabel(r'U_2/V')
 
 plt.plot(x,y,'.',x,reg1,'r-')
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 
 plt.show()
 fig.savefig('plot{}.pdf'.format(name))
@@ -59,11 +62,13 @@ A = np.vstack([x, np.ones(len(x))]).T
 w2 = np.linalg.lstsq(A,y)[0] # obtaining the parameters
 print(w2[0],w2[1])
 reg2 = w2[0]*x + w2[1]
-fig.suptitle('Messung 100kV')
-plt.xlabel('U1/V')
-plt.ylabel('U2/V')
+fig.suptitle(r'Messung 2,97  k\Omega')
+plt.xlabel(r'U_1/V')
+plt.ylabel(r'U_2/V')
 
 plt.plot(x,y,'.',x,reg2,'r-')
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 plt.show()
 fig.savefig('plot{}.pdf'.format(name))
 fig.savefig('plot{}.pgf'.format(name))
